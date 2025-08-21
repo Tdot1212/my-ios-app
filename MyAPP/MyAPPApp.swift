14bf0d638e02716d19b331c6427f99591ae74f35
Tdot1212 <131514658+Tdot1212@users.noreply.github.com>
2025-08-21 12:20:18 +0800

    Initial Commit

---
+++ MyAPP/MyAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAPPApp.swift
+//  MyAPP
+//
+//  Created by Tosh Yagishita on 21/8/2025.
+//
+
+import SwiftUI
+
+@main
+struct MyAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyAPPTests/MyAPPTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyAPPTests.swift
+//  MyAPPTests
+//
+//  Created by Tosh Yagishita on 21/8/2025.
+//
+
+import Testing
+@testable import MyAPP
+
+struct MyAPPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
